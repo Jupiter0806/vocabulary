fbb31fd13c8304a130ff4854e39f3bbd70bd9bc0
jupyter <jupiterli0806@gmail.com>
2017-09-15 17:27:56 +1000

    Beautify code

--- index.js
+++ index.js
@@ -1,176 +1,176 @@
-'use strict';
+/* global responsiveVoice */
 
-var currentWord = '';
+var currentWord = "";
 var index = 0;
 var wordList = [
-        'hello',
-        'world',
-        'you',
-        'pretty'
-    ];
+	"hello",
+	"world",
+	"you",
+	"pretty"
+];
 var needReviewWordList = [];
 
-var reading_currentWord = '';
+var reading_currentWord = "";
 var reading_index = 0;
 var reading_wordList = [
-    'hello',
-    'world',
-    'you',
-    'pretty'
+	"hello",
+	"world",
+	"you",
+	"pretty"
 ];
 var reading_needReviewWordList = [];
 
 var todayWordList = [];
 
-function getNextWord() {
-    return wordList[index++];
+function getNextWord () {
+	return wordList[index++];
 }
 
-function goToNextWord() {
-    currentWord = getNextWord();
-    $('#word').text(currentWord).hide();
+function goToNextWord () {
+	currentWord = getNextWord();
+	$("#word").text(currentWord).hide();
 }
 
-function reorderWordList(wordList) {
-    var i;
-    var index1;
-    var index2;
-    var temp;
+function reorderWordList (wordList) {
+	var i;
+	var index1;
+	var index2;
+	var temp;
 
-    for (i = 0; i < wordList.length; i += 1) {
-        index1 = Math.floor(Math.random() * wordList.length);
-        index2 = Math.floor(Math.random() * wordList.length);
+	for (i = 0; i < wordList.length; i += 1) {
+		index1 = Math.floor(Math.random() * wordList.length);
+		index2 = Math.floor(Math.random() * wordList.length);
 
-        if (index1 !== index2) {
-            temp = wordList[index1];
-            wordList[index1] = wordList[index2];
-            wordList[index2] = temp;
-        }
-    }
+		if (index1 !== index2) {
+			temp = wordList[index1];
+			wordList[index1] = wordList[index2];
+			wordList[index2] = temp;
+		}
+	}
 }
 
-function setUpWordList() {
-    $('#wordList').html(wordList.join('<br/>')).hide();
-    $('#reading_word_list').html(reading_wordList.join('<br/>')).hide();
+function setUpWordList () {
+	$("#wordList").html(wordList.join("<br/>")).hide();
+	$("#reading_word_list").html(reading_wordList.join("<br/>")).hide();
 
-    $('#reviewWordList').hide();
-    $('#reading_review_word_list').hide();
+	$("#reviewWordList").hide();
+	$("#reading_review_word_list").hide();
 }
 
-function goToNextReadingWord() {
-    reading_currentWord = reading_wordList[reading_index++];
-    $('#reading_word').text(reading_currentWord);
+function goToNextReadingWord () {
+	reading_currentWord = reading_wordList[reading_index++];
+	$("#reading_word").text(reading_currentWord);
 }
 
-function init() {
-    reorderWordList(wordList);
-    reorderWordList(reading_wordList);    
-    goToNextWord();  
-    goToNextReadingWord();
-    setUpWordList();  
+function init () {
+	reorderWordList(wordList);
+	reorderWordList(reading_wordList);
+	goToNextWord();
+	goToNextReadingWord();
+	setUpWordList();
 }
 
-function getWordDefinition(word) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "http://localhost:8080/word?word=" + word, true);
+function getWordDefinition (word) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", "http://localhost:8080/word?word=" + word, true);
 
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            var res = JSON.parse(xmlHttp.responseText)
-            console.log("Success: ", res);
-            $('#word_result').html(res);
-        }
-    }
-    xmlHttp.send(null);
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+			var res = JSON.parse(xmlHttp.responseText);
+			console.log("Success: ", res);
+			$("#word_result").html(res);
+		}
+	};
+	xmlHttp.send(null);
 }
 
-$(document).ready(function (){
-    init();
+$(document).ready(function () {
+	init();
 
-    $('#gspeech').on('click', function(){
-        var text = $('input[name="text"]').val();
-        responsiveVoice.speak("" + text + "");
-        // http://responsivevoice.org/
-    });
+	$("#gspeech").on("click", function () {
+		var text = $("input[name=\"text\"]").val();
+		responsiveVoice.speak("" + text + "");
+		// http://responsivevoice.org/
+	});
 
-    $('#search_word').on('click', function () {
-        var inputWord = $('input[name="text"]').val();
-        if (inputWord) {
-            getWordDefinition(inputWord);
-            if (todayWordList.indexOf(inputWord) < 0) {
-                todayWordList.push(inputWord);
-            }
-        }
-    });
+	$("#search_word").on("click", function () {
+		var inputWord = $("input[name=\"text\"]").val();
+		if (inputWord) {
+			getWordDefinition(inputWord);
+			if (todayWordList.indexOf(inputWord) < 0) {
+				todayWordList.push(inputWord);
+			}
+		}
+	});
 
-    $('#output_today_word_list').on('click', function () {
-        $('#today_word_list').text(todayWordList.join(';'));
-    });
+	$("#output_today_word_list").on("click", function () {
+		$("#today_word_list").text(todayWordList.join(";"));
+	});
 
-    $('#speak').on('click', function () {
-        if (currentWord) {
-            responsiveVoice.speak("" + currentWord + "");
-        }
-    });
+	$("#speak").on("click", function () {
+		if (currentWord) {
+			responsiveVoice.speak("" + currentWord + "");
+		}
+	});
 
-    $('#check').on('click', function () {
-        var inputWord = $('input[name="wordinput"]').val();
-        $('#check_result').text(inputWord === currentWord);
-        
-    });
+	$("#check").on("click", function () {
+		var inputWord = $("input[name=\"wordinput\"]").val();
+		$("#check_result").text(inputWord === currentWord);
 
-    $('#show').on('click', function() {
-        $('#word').toggle();
-        if (needReviewWordList.indexOf(currentWord) < 0) {
-            needReviewWordList.push(currentWord);
-        }
-    });
-    
-    $('#next').on('click', function(){
-        goToNextWord();
-        $('#check_result').text();
-    });
+	});
 
-    $('#learn_today_words').on('click', function () {
-        wordList = todayWordList;
-        init();
-    });
+	$("#show").on("click", function () {
+		$("#word").toggle();
+		if (needReviewWordList.indexOf(currentWord) < 0) {
+			needReviewWordList.push(currentWord);
+		}
+	});
 
-    $('#showWordList').on('click', function () {
-        $('#wordList').toggle();
-    });
+	$("#next").on("click", function () {
+		goToNextWord();
+		$("#check_result").text();
+	});
 
-    $('#showReviewWordList').on('click', function () {
-        $('#reviewWordList').html(needReviewWordList.join('<br/>')).toggle();;
-    });
+	$("#learn_today_words").on("click", function () {
+		wordList = todayWordList;
+		init();
+	});
 
-    $('#reading_speak').on('click', function () {
-        if (reading_currentWord) {
-            responsiveVoice.speak("" + reading_currentWord + "");
-        }
-    });
+	$("#showWordList").on("click", function () {
+		$("#wordList").toggle();
+	});
 
-    $('#reading_not_know').on('click', function () {
-        if (reading_needReviewWordList.indexOf(reading_currentWord) < 0) {
-            reading_needReviewWordList.push(reading_currentWord);
-        }
-    });
+	$("#showReviewWordList").on("click", function () {
+		$("#reviewWordList").html(needReviewWordList.join("<br/>")).toggle();
+	});
 
-    $('#reading_next').on('click', function () {
-        goToNextReadingWord();
-    });
+	$("#reading_speak").on("click", function () {
+		if (reading_currentWord) {
+			responsiveVoice.speak("" + reading_currentWord + "");
+		}
+	});
 
-    $('#reading_learn_today_words').on('click', function () {
-        reading_wordList = todayWordList;
-        init();
-    });
+	$("#reading_not_know").on("click", function () {
+		if (reading_needReviewWordList.indexOf(reading_currentWord) < 0) {
+			reading_needReviewWordList.push(reading_currentWord);
+		}
+	});
 
-    $('#show_reading_word_list').on('click', function () {
-        $('#reading_word_list').toggle();
-    });
+	$("#reading_next").on("click", function () {
+		goToNextReadingWord();
+	});
 
-    $('#show_reading_review_word_list').on('click', function () {
-        $('#reading_review_word_list').html(reading_needReviewWordList.join('<br/>')).toggle();;        
-    });
+	$("#reading_learn_today_words").on("click", function () {
+		reading_wordList = todayWordList;
+		init();
+	});
+
+	$("#show_reading_word_list").on("click", function () {
+		$("#reading_word_list").toggle();
+	});
+
+	$("#show_reading_review_word_list").on("click", function () {
+		$("#reading_review_word_list").html(reading_needReviewWordList.join("<br/>")).toggle();
+	});
 
 });
--- node.js
+++ node.js
@@ -1,83 +1,91 @@
-var http = require('http');
-var request = require('request');
-var url = require('url');
-var fs = require('fs');
+var http = require("http");
+var request = require("request");
+var url = require("url");
+var fs = require("fs");
 
-function getWordDefinition (word, callback) {
-    var options = {
-        url: "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + word,
-        headers: {
-            "Accept" : "application/json",
-            "app_id" : "1dc10a7e",
-            "app_key" : "fa6c67f08dd0f9aab2a63beb1b30694f"
-        }
-    };
-    
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var info = getDefinitionFromResponse(JSON.parse(body));
-            console.log(info);
-            callback(info);
-        } else {
-            console.log("Error: ", error, response);
-            callback(null);
-        }
-    });
+function getWordDefinition(word, callback) {
+	var options = {
+		url: "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + word,
+		headers: {
+			"Accept": "application/json",
+			"app_id": "1dc10a7e",
+			"app_key": "fa6c67f08dd0f9aab2a63beb1b30694f"
+		}
+	};
+
+	request(options, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var info = getDefinitionFromResponse(JSON.parse(body));
+			console.log(info);
+			callback(info);
+		} else {
+			console.log("Error: ", error, response);
+			callback(null);
+		}
+	});
 }
 
 function getDefinitionFromResponse(response) {
-    var res = [];
-    for (var i = 0; i < response.results.length; i++) {
-        for (var j = 0; j < response.results[i].lexicalEntries.length; j++) {
-            for (var z = 0; z < response.results[i].lexicalEntries[j].entries.length; z++) {
-                for (var w = 0; w < response.results[i].lexicalEntries[j].entries[z].senses.length; w++) {
-                    res.push({
-                        definitions: response.results[i].lexicalEntries[j].entries[z].senses[w].definitions,
-                        examples: response.results[i].lexicalEntries[j].entries[z].senses[w].examples
-                    })
-                }
-            }
-        }
-    }
-    return res;
+	var res = [];
+	for (var i = 0; i < response.results.length; i++) {
+		for (var j = 0; j < response.results[i].lexicalEntries.length; j++) {
+			for (var z = 0; z < response.results[i].lexicalEntries[j].entries.length; z++) {
+				for (var w = 0; w < response.results[i].lexicalEntries[j].entries[z].senses.length; w++) {
+					res.push({
+						definitions: response.results[i].lexicalEntries[j].entries[z].senses[w].definitions,
+						examples: response.results[i].lexicalEntries[j].entries[z].senses[w].examples
+					});
+				}
+			}
+		}
+	}
+	return res;
 }
 
 
 http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var word = q.query.word;
-    var pathname = q.pathname;
+	var q = url.parse(req.url, true);
+	var word = q.query.word;
+	var pathname = q.pathname;
 
-    console.log(pathname);
+	console.log(pathname);
 
-    if (pathname === '/word') {
-        console.log("word is ", word);
-        getWordDefinition(word, function (data) {
-            if (data) {
-                res.writeHead(200, {'Content-Type': 'text/json'});
-                res.write(JSON.stringify(data));
-                res.end();
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/json'});
-                res.write("{}");
-                res.end();
-            }
-        });
-    } else if (pathname === '/index.js') {
-        fs.readFile(pathname.slice(1), function (error, data) {
-            if (error) throw error;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        })
-    } else if (pathname === '/'){
-        fs.readFile('index.html', function (error, data) {
-            if (error) throw error;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        });
-    }
+	if (pathname === "/word") {
+		console.log("word is ", word);
+		getWordDefinition(word, function (data) {
+			if (data) {
+				res.writeHead(200, {
+					"Content-Type": "text/json"
+				});
+				res.write(JSON.stringify(data));
+				res.end();
+			} else {
+				res.writeHead(200, {
+					"Content-Type": "text/json"
+				});
+				res.write("{}");
+				res.end();
+			}
+		});
+	} else if (pathname === "/index.js") {
+		fs.readFile(pathname.slice(1), function (error, data) {
+			if (error) throw error;
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			});
+			res.write(data);
+			res.end();
+		});
+	} else if (pathname === "/") {
+		fs.readFile("index.html", function (error, data) {
+			if (error) throw error;
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			});
+			res.write(data);
+			res.end();
+		});
+	}
 
 }).listen(8080);
 
